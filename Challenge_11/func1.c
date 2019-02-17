71094ac3b4dc2b902938eb1aaad8f84d3407099a
EmanGamalmuhammad <Eman.Gamal@avelabs.com>
2019-02-17 19:13:27 +0200

    challeng_11

---
+++ Challenge_11/func1.c
@@ -0,0 +1,11 @@
+/*
+ * func1.c
+ *
+ *  Created on: Feb 17, 2019
+ *      Author: AVE-LABS-50
+ */
+
+void func1(void)
+{
+	printf("hello func1\n");
+}
---
+++ Challenge_11/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 17, 2019
+ *      Author: AVE-LABS-50
+ */
+
+#include "func1.h"
+#include "func2.h"
+
+main()
+{
+ func1();
+ func2();
+}
